c068abda81fa866c9d5ac293e967af2ff4cd85f1
Semenov Sergei <dp120291ssv@gmail.com>
2020-02-06 21:17:17 +0200

    add pom xml

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -5,11 +5,11 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GettingStarted {
+
 	@Test
 	public void testGoogleSearch() throws InterruptedException {
 		// Optional. If not specified, WebDriver searches the PATH for chromedriver.
 		System.setProperty("webdriver.chrome.driver", "/Users/privatceb/Documents/SELENIUM/selenium/src/main/resources/drivers/chromedriver");
-
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.com/");
 		Thread.sleep(5000);  // Let the user actually see something!
