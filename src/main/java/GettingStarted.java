b2ae4326b5bb9a0cb6e13ee61678ec740b0b9a3e
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-05 19:46:13 +0300

    update first test

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -1,22 +1,46 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GettingStarted {
 
+
 	@Test
 	public void testGoogleSearch() throws InterruptedException {
-		// Optional. If not specified, WebDriver searches the PATH for chromedriver.
-		System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+
 		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		driver.get("http://www.google.com/");
-		Thread.sleep(5000);  // Let the user actually see something!
 		WebElement searchBox = driver.findElement(By.name("q"));
 		searchBox.sendKeys("ChromeDriver");
 		searchBox.submit();
-		Thread.sleep(5000);  // Let the user actually see something!
 		driver.quit();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	}
 }
