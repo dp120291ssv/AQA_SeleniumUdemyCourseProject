622317b540fc0eae481b551ae64235b80c439296
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-05 20:33:28 +0300

    main methods

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -1,6 +1,10 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class GettingStarted {
 
@@ -11,14 +15,36 @@ public class GettingStarted {
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 		driver.get("http://www.google.com/");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
+
+//		driver.navigate().to("http://www.google.com/");
+//		Set<String> oldWindow = driver.getWindowHandles();
+//		System.out.println("oldWindow->" + oldWindow);
+//
+//		((JavascriptExecutor)driver).executeScript("window.open()");
+//		ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+//
+//		driver.switchTo().window(tabs.get(1));
+//		driver.get("https://selenium.dev");
+//
+//		Set<String> newWindow = driver.getWindowHandles();
+//		System.out.println("newWindow->" + newWindow);
+
+//		WebElement searchBox = driver.findElement(By.name("q"));
+//		searchBox.sendKeys("ChromeDriver");
+//		searchBox.submit();
+
+//		String currentURL = driver.getCurrentUrl();
+//		System.out.println("currentURL->" + currentURL);
+//		Assert.assertEquals(currentURL, "http://www.google.com/");
+
+//		String currentTitle = driver.getTitle();
+//		System.out.println("currentTitle->" + currentTitle);
+//		Assert.assertEquals(currentTitle, "ChromeDriver - Поиск в Google");
+
+//		String pageSource = driver.getPageSource();
+//		System.out.println("pageSource->" + pageSource);
+
 		driver.quit();
-
-
-
-
 
 
 
