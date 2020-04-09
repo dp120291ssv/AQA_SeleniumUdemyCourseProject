ac05815d7aa7f08ba55973ce78ba36977b0555d1
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-09 11:15:44 +0300

    add page actions

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -2,18 +2,37 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class GettingStarted {
+import java.util.Set;
 
+public class GettingStarted {
 
 	@Test
 	public void testGoogleSearch() throws InterruptedException {
 
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-		driver.get("http://www.google.com/");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
-		driver.quit();
+//		WebElement searchBox = driver.findElement(By.name("q"));
+//		searchBox.sendKeys("ChromeDriver");
+//		searchBox.submit();
+
+ 		driver.get("https://github.com/dp120291ssv");
+
+		Cookie someNameForCookie = new Cookie("user_session", "m0IXeaEwYZCmde2eWbGiDaGv8G2HZtx5sV-lwS-GDczCWQHI");
+		driver.manage().addCookie(someNameForCookie);
+		driver.navigate().refresh();
+
+		Set<Cookie> allCookies = driver.manage().getCookies();
+		System.out.println("allCookies->" + allCookies);
+
+		Cookie userLogin = driver.manage().getCookieNamed("dotcom_user");
+		System.out.println("userLogin->" + userLogin);
+
+		driver.manage().deleteCookie(userLogin);
+		System.out.println("deleteCookie userLogin ->" + driver.manage().getCookies());
+
+		driver.manage().deleteAllCookies();
+		System.out.println("deleteAllCookies->" + driver.manage().getCookies());
+
+//		driver.quit();
 	}
 }
