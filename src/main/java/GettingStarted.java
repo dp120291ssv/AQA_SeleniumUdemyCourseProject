983772b69f3e9cb9813e5ccb8c8537080eb0775f
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-10 21:11:44 +0300

    add some example code

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -10,10 +10,31 @@ public class GettingStarted {
 
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+
+//		driver.manage().window().setSize(new Dimension(1000, 1000));
+//
+//		driver.manage().window().maximize();
+//
+//		driver.manage().window().fullscreen();
+//
+//		System.out.println("getSize->" + driver.manage().window().getSize());
+//
+//		System.out.println("getPosition->" + driver.manage().window().getPosition());
+//
+//		driver.manage().window().setPosition(new Point(50, 50));
+
+//		Set<String> logsType = driver.manage().logs().getAvailableLogTypes();
+//		System.out.println("logsType->" + logsType);
+//
+//		LogEntries logs = driver.manage().logs().get(LogType.BROWSER);
+//		for (LogEntry entry : logs) {
+//			System.out.println("logs-> " + entry);
+//		}
+
 		driver.get("http://www.google.com/");
 		WebElement searchBox = driver.findElement(By.name("q"));
 		searchBox.sendKeys("ChromeDriver");
 		searchBox.submit();
-		driver.quit();
+//		driver.quit();
 	}
 }
