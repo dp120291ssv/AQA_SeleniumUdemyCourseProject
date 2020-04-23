86d8df894e3db95222cc42985aff3c6f753593ba
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-23 12:15:05 +0300

    add test

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -1,6 +1,9 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GettingStarted {
 
@@ -10,10 +13,19 @@ public class GettingStarted {
 
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-		driver.get("http://www.google.com/");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
-		driver.quit();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		driver.navigate().to("https://next.privat24.ua/money-transfer/card?lang=en");
+
+		WebElement addCommentLink = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[@data-qa-node='toggle-comment']")));
+		addCommentLink.click();
+
+		WebElement commentField = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//textarea[@data-qa-node='comment']")));
+
+		WebElement closeElement = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@title='Close']")));
+		closeElement.click();
+
+		Assert.assertTrue(wait.until(ExpectedConditions.stalenessOf(commentField)));
 	}
 }
