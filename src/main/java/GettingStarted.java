b581398151ca8747b0566f539e5a90ed1279b505
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-28 11:15:40 +0300

    add test

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -1,8 +1,13 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GettingStarted {
+
+
 
 
 	@Test
@@ -10,10 +15,46 @@ public class GettingStarted {
 
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-		driver.get("http://www.google.com/");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
-		driver.quit();
+
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		driver.navigate().to("https://next.privat24.ua/?lang=en");
+
+		WebElement buttonLogin = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[@data-qa-node='login']")));
+		buttonLogin.click();
+
+		WebElement frameLogin = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@src='https://login-widget.privat24.ua/']")));
+
+		driver.switchTo().frame(frameLogin);
+
+		WebElement buttonContinue = wait.until(ExpectedConditions
+			.presenceOfElementLocated(By.xpath("//button[@type='submit']")));
+
+		buttonContinue.click();
+
+		wait.until(ExpectedConditions
+			.visibilityOfElementLocated(By.xpath("//div[contains(text(), 'Incorrect value was entered')]")));
+
+
+		driver.navigate().to("https://jsbin.com/kotirezuvo/edit?html,output");
+
+		WebElement mainFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@class='stretch']")));
+		driver.switchTo().frame(mainFrame);
+
+		WebElement outputFrame = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//iframe[@name='JS Bin Output ']")));
+		driver.switchTo().frame(outputFrame);
+
+		WebElement inputNumberOfVtdeo = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@id='seleniumCourse']")));
+		inputNumberOfVtdeo.sendKeys("99");
+
+		driver.switchTo().defaultContent();
+
+		Assert.assertEquals(driver.getTitle(), "JS Bin - Collaborative JavaScript Debugging");
+
+
+
+
+
+
 	}
 }
