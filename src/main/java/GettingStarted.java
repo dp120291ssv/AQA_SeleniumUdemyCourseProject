6c1429a57f0f864052034a9cab799e1db6798c63
Semenov Sergei <dp120291ssv@gmail.com>
2020-04-29 19:15:08 +0300

    add test

--- src/main/java/GettingStarted.java
+++ src/main/java/GettingStarted.java
@@ -1,6 +1,9 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GettingStarted {
 
@@ -10,10 +13,13 @@ public class GettingStarted {
 
 		WebDriver driver = new ChromeDriver();
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-		driver.get("http://www.google.com/");
-		WebElement searchBox = driver.findElement(By.name("q"));
-		searchBox.sendKeys("ChromeDriver");
-		searchBox.submit();
-		driver.quit();
+
+		driver.navigate().to("https://www.facebook.com/login.php");
+		WebElement loginButton = driver.findElement(By.xpath("//button[@id='loginbutton']"));
+		loginButton.click();
+
+//		WebElement createAccButton = driver.findElement(By.xpath("(//a[@role='button'])[2]"));
+//		createAccButton.click();
+
 	}
 }
