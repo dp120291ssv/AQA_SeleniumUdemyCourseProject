771bce47f66dcd555e46a2aaa77f0d1dfdcf001c
Semenov Sergei <dp120291ssv@gmail.com>
2020-05-26 18:30:21 +0300

    some changes

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -4,4 +4,5 @@ public class Config {
 
     public static String BROWSER_NAME = "chrome";
     public static boolean CLEAR_COOKIES = true;
+    public static String IS_NEEDED_SCREENSHOT = "whenFailure";
 }
--- src/main/java/common/Listener.java
+++ src/main/java/common/Listener.java
@@ -1,20 +1,26 @@
 package common;
 
 import io.qameta.allure.Attachment;
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import java.io.File;
+import java.io.IOException;
 import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
 
 public class Listener implements TestWatcher, BeforeAllCallback, AfterEachCallback {
 
-	CommonActions commonActions;
+	CommonActions commonActions = new CommonActions();
 	protected WebDriver driver = commonActions.driver;
 	private static final Logger LOGGER = LoggerFactory.getLogger(Listener.class);
 
@@ -31,11 +37,31 @@ public class Listener implements TestWatcher, BeforeAllCallback, AfterEachCallba
 
 	@Override
 	public void testFailed(ExtensionContext context, Throwable cause) {
+//		captureScreenshot();
 		attachScreenshotToAllure();
 	}
 
 	@Attachment(value = "Attachment Screenshot", type = "image/png")
 	public byte[] attachScreenshotToAllure() {
 		return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+	}
+
+	@Rule
+	public TestRule screenshotRule = new org.junit.rules.TestWatcher() {
+		@Override
+		protected void failed(Throwable e, Description description) {
+			captureScreenshot(description.getMethodName());
+		}
+	};
+
+	private void captureScreenshot(String name) {
+		File screenshot = ((TakesScreenshot) driver)
+			.getScreenshotAs(OutputType.FILE);
+		String path = "./screenshots/" + screenshot.getName();
+		try {
+			FileUtils.copyFile(screenshot, new File(path));
+		} catch (IOException e) {
+
+		}
 	}
 }
