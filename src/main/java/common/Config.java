4330223a3b4a15a4593c6aa77907a81648e5cda7
Semenov Sergei <dp120291ssv@gmail.com>
2020-05-26 18:30:43 +0300

    some changes

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -4,5 +4,4 @@ public class Config {
 
     public static String BROWSER_NAME = "chrome";
     public static boolean CLEAR_COOKIES = true;
-    public static String IS_NEEDED_SCREENSHOT = "whenFailure";
 }
