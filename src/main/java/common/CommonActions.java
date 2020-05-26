8506a737dda31d571260cb9e36ee498960ef5f28
Semenov Sergei <dp120291ssv@gmail.com>
2020-05-26 15:31:11 +0300

    add screenshot and Listener 3

--- src/main/java/common/CommonActions.java
+++ src/main/java/common/CommonActions.java
@@ -1,19 +1,8 @@
 package common;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Rule;
-import org.junit.rules.TestRule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.Description;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.io.File;
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-
 import static common.Config.BROWSER_NAME;
 import static constants.Constants.TimeoutVariables.IMPLICIT_WAIT;
 
@@ -32,30 +21,10 @@ public class CommonActions {
         driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
         driver.manage().window().maximize();
     }
-
     // static method to create instance of Singleton class
     public static CommonActions getInstance() {
         if (instance == null)
             instance = new CommonActions();
         return instance;
     }
-
-	@Rule
-	public TestRule screenshotRule = new TestWatcher() {
-		@Override
-		protected void failed(Throwable e, Description description) {
-			captureScreenshot(description.getMethodName());
-		}
-	};
-
-	private void captureScreenshot(String name) {
-		File screenshot = ((TakesScreenshot) driver)
-			.getScreenshotAs(OutputType.FILE);
-		String path = "./target/screenshots/" + screenshot.getName();
-		try {
-			FileUtils.copyFile(screenshot, new File(path));
-		} catch (IOException e) {
-
-		}
-	}
 }
